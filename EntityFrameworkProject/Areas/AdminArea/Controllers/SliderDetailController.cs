efebd66a9ba2fd403575c40952ea56f9319f6511
Orkhansha <orkhansha@code.edu.az>
2022-10-11 00:44:08 +0400

    Added Slider-Text-Section

---
+++ EntityFrameworkProject/Areas/AdminArea/Controllers/SliderDetailController.cs
@@ -0,0 +1,26 @@
+﻿using EntityFrameworkProject.Data;
+using EntityFrameworkProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkProject.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class SliderDetailController : Controller
+    {
+        private readonly AppDbContext _context;
+        public SliderDetailController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<SliderDetail> sliderDetails = await _context.SliderDetails.Where(m => !m.IsDeleted).ToListAsync();
+            return View(sliderDetails);
+        }
+    }
+}
